8d599a73f1240163b697512225354418a5f684a5
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-08 18:32:20 -0300

    Start refactoring by moving Board to its separate class and use it inside Game

---
+++ src/classes/board.rb
@@ -0,0 +1,22 @@
+class Board
+  attr_reader :state
+
+  def initialize
+    @state = %w[0 1 2 3 4 5 6 7 8]
+  end
+
+  def game_is_over
+    [state[0], state[1], state[2]].uniq.length == 1 ||
+    [state[3], state[4], state[5]].uniq.length == 1 ||
+    [state[6], state[7], state[8]].uniq.length == 1 ||
+    [state[0], state[3], state[6]].uniq.length == 1 ||
+    [state[1], state[4], state[7]].uniq.length == 1 ||
+    [state[2], state[5], state[8]].uniq.length == 1 ||
+    [state[0], state[4], state[8]].uniq.length == 1 ||
+    [state[2], state[4], state[6]].uniq.length == 1
+  end
+
+  def to_s
+    "\s#{state[0]} | #{state[1]} | #{state[2]} \n===+===+===\n #{state[3]} | #{state[4]} | #{state[5]} \n===+===+===\n #{state[6]} | #{state[7]} | #{state[8]} \n"
+  end
+end
---
+++ src/classes/game.rb
@@ -0,0 +1,86 @@
+class Game
+  require_relative "./board.rb"
+
+  def initialize
+    @board = Board.new
+    @com = 'X' # the computer's marker
+    @hum = 'O' # the user's marker
+  end
+
+  def start_game
+    # start by printing the board
+    puts @board
+    puts 'Enter [0-8]:'
+    # loop through until the game was won or tied
+    until @board.game_is_over || tie(@board.state)
+      get_human_spot
+      eval_board if @board.game_is_over && !tie(@board.state)
+      puts @board
+    end
+    puts 'Game over'
+  end
+
+  def get_human_spot
+    spot = nil
+    until spot
+      spot = gets.chomp.to_i
+      if @board.state[spot] != 'X' && @board.state[spot] != 'O'
+        @board.state[spot] = @hum
+      else
+        spot = nil
+      end
+    end
+  end
+
+  def eval_board
+    spot = nil
+    until spot
+      if @board.state[4] == '4'
+        spot = 4
+        @board.state[spot] = @com
+      else
+        spot = get_best_move(@board.state, @com)
+        if @board.state[spot] != 'X' && @board.state[spot] != 'O'
+          @board.state[spot] = @com
+        else
+          spot = nil
+        end
+      end
+    end
+  end
+
+  def get_best_move(board, _next_player, _depth = 0, _best_score = {})
+    available_spaces = []
+    best_move = nil
+    board.each do |s|
+      available_spaces << s if s != 'X' && s != 'O'
+    end
+    available_spaces.each do |as|
+      board[as.to_i] = @com
+      if @board.game_is_over
+        best_move = as.to_i
+        board[as.to_i] = as
+        return best_move
+      else
+        board[as.to_i] = @hum
+        if @board.game_is_over
+          best_move = as.to_i
+          board[as.to_i] = as
+          return best_move
+        else
+          board[as.to_i] = as
+        end
+      end
+    end
+    return best_move if best_move
+
+    n = rand(0..available_spaces.count)
+    available_spaces[n].to_i
+  end
+
+  def tie(b)
+    b.all? { |s| %w[X O].include?(s) }
+  end
+end
+
+Game.new.start_game
