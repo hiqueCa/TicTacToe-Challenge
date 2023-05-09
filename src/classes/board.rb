27a6bf8f8ed5533c46927633c53389a77a1012ec
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-09 08:17:20 -0300

    Rename validation method for board

--- src/classes/board.rb
+++ src/classes/board.rb
@@ -20,7 +20,7 @@ class Board
     "\s#{state[0]} | #{state[1]} | #{state[2]} \n===+===+===\n #{state[3]} | #{state[4]} | #{state[5]} \n===+===+===\n #{state[6]} | #{state[7]} | #{state[8]} \n"
   end
 
-  def valid_board_position?(input_position)
+  def valid_position?(input_position)
     state[input_position.to_i] != 'X' && state[input_position.to_i] != 'O'
   end
 end
