ef7e9fc2e0aabf95471d46d3aa8733541c7a9595
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-11 08:22:26 -0300

    Add simplecov gem to Gemfile and use it to measure test coverage of classes and files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+require 'simplecov'
+SimpleCov.start
+
 require_relative '../src/classes/player_tree/player'
 require_relative '../src/classes/player_tree/human'
 require_relative '../src/classes/player_tree/computer'
 require_relative '../src/classes/move'
 require_relative '../src/classes/board'
 require 'pry'
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    SimpleCov.result.format!
+  end
+end
