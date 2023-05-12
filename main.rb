40036aa6ee4419eea31e6b258ac5491bf154776e
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-12 09:17:10 -0300

    Add possibility to keep playing based on user input

--- main.rb
+++ main.rb
@@ -1,3 +1,21 @@
 require_relative './src/classes/game'
 
-Game.new.start
+playing = true
+
+while playing
+  Game.new.start
+
+  puts "Do you want to keep playing? (Y / N)"
+
+  keep_playing = gets.chomp
+
+  until (keep_playing == "Y" || keep_playing == "N")
+    puts "Do you wnat to keep playing? (Y / N)"
+
+    keep_playing = gets.chomp
+  end
+
+  if keep_playing == "N"
+    playing = false
+  end
+end
