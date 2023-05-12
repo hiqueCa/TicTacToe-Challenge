1ff18a900b4ac65087dda37f343d4bd0324246fb
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-12 07:46:20 -0300

    Refactor Computer to use try_move method

--- src/classes/player_tree/computer.rb
+++ src/classes/player_tree/computer.rb
@@ -12,7 +12,7 @@ class Computer < Player
     board = game.board
 
     if board.is_central_dominance_spot_available?
-      input_position = '4'
+      input_position = 4
     else
       input_position = define_best_available_move(game, next_player)
     end
@@ -24,21 +24,24 @@ class Computer < Player
 
   def define_best_available_move(game, next_player)
     board = game.board
-    best_move = nil
     available_spots_for_current_try = board.available_spots
+
+    best_move = randomize_move_position_based_on(available_spots_for_current_try)
 
     available_spots_for_current_try.each do |available_spot|
       board.state[available_spot.to_i] = next_player.marker
 
       if game.has_a_winner?
         best_move = available_spot.to_i
-        return best_move
       end
 
       board.state[available_spot.to_i] = available_spot
     end
 
-    random_move_position = rand(0..available_spots_for_current_try.count)
-    available_spots_for_current_try[random_move_position]
+    best_move
+  end
+
+  def randomize_move_position_based_on(available_spots)
+    rand(0..available_spots.count)
   end
 end
