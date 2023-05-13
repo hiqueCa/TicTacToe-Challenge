e4bbdb7a3905125c1f2714aea5d514a900df982a
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-13 09:52:14 -0300

    Add name to human and computer

--- src/classes/player_tree/human.rb
+++ src/classes/player_tree/human.rb
@@ -2,8 +2,8 @@ require_relative 'player'
 
 class Human < Player
 
-  def initialize(marker)
-    super(marker)
+  def initialize(marker, name)
+    super(marker, name)
     @type = Player::AVAILABLE_PLAYER_TYPES['HUMAN']
   end
 end
