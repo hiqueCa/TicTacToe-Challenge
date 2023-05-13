ff081da4ba8a46e8ed3ce4c803d648f94d332dee
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-13 10:14:31 -0300

    Move main constants to GameConstants::Main module and use them

--- main.rb
+++ main.rb
@@ -1,17 +1,9 @@
 require_relative './src/classes/game'
 require_relative './src/modules/validator'
+require_relative './src/modules/game_constants/main'
 
 include Validator
-
-PLAY_AGAIN_INPUTS_PLAYING_MAPPER = {
-  "Y" => true,
-  "N" => false,
-}
-
-PLAY_AGAIN_MESSAGE = "\nDo you want to play again? (Y / N)"
-
-INTRODUCTORY_DEFINE_GAME_TYPE_MESSAGE = "Hello! What kind of game would you want to play?: (CPU_CPU, HUMAN_HUMAN, HUMAN_CPU)"
-RETRY_DEFINE_GAME_TYPE_MESSAGE = "Please, define a valid game type to play: (CPU_CPU, HUMAN_HUMAN, HUMAN_CPU)"
+include GameConstants::Main
 
 def game_type_input_valid(game_type)
   validate?(
---
+++ src/modules/game_constants/main.rb
@@ -0,0 +1,13 @@
+module GameConstants
+  module Main
+    PLAY_AGAIN_INPUTS_PLAYING_MAPPER = {
+      "Y" => true,
+      "N" => false,
+    }
+    
+    PLAY_AGAIN_MESSAGE = "\nDo you want to play again? (Y / N)"
+    INTRODUCTORY_DEFINE_GAME_TYPE_MESSAGE = "Hello! What kind of game would you want to play?: (CPU_CPU, HUMAN_HUMAN, HUMAN_CPU)"
+    RETRY_DEFINE_GAME_TYPE_MESSAGE = "Please, define a valid game type to play: (CPU_CPU, HUMAN_HUMAN, HUMAN_CPU)"
+    
+  end
+end
