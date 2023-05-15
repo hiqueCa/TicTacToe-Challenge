4902594e999fa06e358d60e81378b9124a20ab57
Henrique Pousa Caltram <henrivai@hotmail.com>
2023-05-15 07:54:35 -0300

    Add more spec coverage by further testing computer class

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -28,32 +28,53 @@ describe Computer do
     let(:computer) { game.player_two }
 
     before do
-      board.state = board_state
+      board.state[4] = central_dominance_spot
     end
 
     subject { computer.make_move(game, player_one) }
 
     context 'when central dominance spot is available on board' do
-      let(:board_state) { %[0 X 2 O 4 5 6 X 8] }
+      let(:central_dominance_spot) { '4' } 
 
       it 'creates a new move trial for the computer' do
         expect { subject }.to change { computer.move_trials.count }.by(1)
       end
 
       it "sets the computer's last_valid_move as the last move trial available" do
+        subject
         expect(computer.last_valid_move).to eq(computer.move_trials.last)
+      end
+
+      it 'defines the central dominance spot as the move for the computer' do
+        subject
+        expect(computer.last_valid_move.valid_position).to eq(4)
       end
     end
 
     context 'when central dominance spot is not available on board' do
-      let(:board_state) { %[0 X 2 O O 5 6 X 8] }
+      let(:central_dominance_spot) { 'X' } 
 
       it 'creates a new move trial for the computer' do
         expect { subject }.to change { computer.move_trials.count }.by(1)
       end
 
       it "sets the computer's last_valid_move as the last move trial available" do
+        subject
         expect(computer.last_valid_move).to eq(computer.move_trials.last)
+      end
+
+      it 'does not set the central dominance spot as the move for the computer' do
+        subject
+        expect(computer.last_valid_move.valid_position).not_to eq(4)
+      end
+
+      it 'calls the Computer.define_best_move method' do
+        expect(computer)
+          .to receive(:define_best_available_move)
+            .with(game, player_one)
+              .and_call_original
+        
+        subject
       end
     end
   end
